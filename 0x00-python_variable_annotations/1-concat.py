1d6b45a9a0cb573b3972dec2be7aad8f4f77f6b9
JosephatNgugi <josephatn10@gmail.com>
2024-04-05 19:20:34 +0300

    write a type annotated function 'concat'.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Takes a string str1 and a string str2 as arguments
+        returns a concatenated string
+    """
+    return str1 + str2
